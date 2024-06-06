6bdb4e8c06b41c9a80810ef717d7b8b45b75ccfe
Silf2 <sylvain.guenet1@gmail.com>
2024-06-06 08:59:49 +0200

    Ajout du GetAllAdvices

---
+++ src/Controller/Advices/GetAllAdvices.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Advices;
+
+use App\Entity\Advice;
+use App\Repository\AdviceRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[AsController]
+final class GetAllAdvices
+{
+    public function __construct(
+        private AdviceRepository $adviceRepository,
+        private SerializerInterface $serializer/*, 
+        private HttpClientInterface $weatherClient*/
+    )
+    {
+    }
+
+    #[Route('/api/advices', name: 'getAllAdvices', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        $advices = $this->adviceRepository->findAll();
+        $jsonAdvices = $this->serializer->serialize($advices, 'json');
+
+        return new JsonResponse($jsonAdvices, Response::HTTP_OK, [], true);
+    }
+}
+
