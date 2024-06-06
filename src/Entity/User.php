5c80d2cc1b735cde4285d236c91e4cf4b7d66a97
Silf2 <sylvain.guenet1@gmail.com>
2024-06-06 09:41:53 +0200

    Modification de l'entité User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,27 +4,34 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180)]
     private ?string $username = null;
 
-    #[ORM\Column(length: 255)]
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
     private ?string $postal_code = null;
-
-    #[ORM\Column]
-    private ?bool $isAdmin = null;
 
     public function getId(): ?int
     {
@@ -43,7 +50,43 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -55,26 +98,31 @@ class User
         return $this;
     }
 
-    public function getPostalCode(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * Get the value of postal_code
+     */ 
+    public function getPostal_code()
     {
         return $this->postal_code;
     }
 
-    public function setPostalCode(string $postal_code): static
+    /**
+     * Set the value of postal_code
+     *
+     * @return  self
+     */ 
+    public function setPostal_code($postal_code)
     {
         $this->postal_code = $postal_code;
-
-        return $this;
-    }
-
-    public function isAdmin(): ?bool
-    {
-        return $this->isAdmin;
-    }
-
-    public function setAdmin(bool $isAdmin): static
-    {
-        $this->isAdmin = $isAdmin;
 
         return $this;
     }
