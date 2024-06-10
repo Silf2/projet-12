fd2dc716a9863b6b46cd15131607b2c66e120f58
Silf2 <sylvain.guenet1@gmail.com>
2024-06-10 12:55:50 +0200

    (update) Password Check

--- src/Controller/User/PostUser.php
+++ src/Controller/User/PostUser.php
@@ -12,26 +12,48 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[AsController]
-final class PostUser{
-
+final class PostUser
+{
     public function __construct(
         private SerializerInterface $serializer,
         private EntityManagerInterface $em,
         private UrlGeneratorInterface $urlGenerator,
-        private UserPasswordHasherInterface $passwordHasher
-    )
-    {
+        private UserPasswordHasherInterface $passwordHasher,
+        private ValidatorInterface $validator
+    ) {
     }
 
-
-    #[Route('/api/user', name:"createUser", methods: ['POST'])]
+    #[Route('/api/user', name: "createUser", methods: ['POST'])]
     public function __invoke(Request $request): JsonResponse
     {
         $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
         $user->setRoles(["ROLE_USER"]);
+
         $password = $user->getPassword();
+        $passwordConstraints = new Assert\Collection([
+            'password' => [
+                new Assert\NotBlank(),
+                new Assert\Length(['min' => 8, 'minMessage' => 'Le mot de passe doit contenir au moins 8 caractères.']),
+                new Assert\Regex(['pattern' => '/[A-Z]/', 'message' => 'Le mot de passe doit contenir au moins une majuscule.']),
+            ]
+        ]);
+
+        $input = ['password' => $password];
+        $violations = $this->validator->validate($input, $passwordConstraints);
+
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[] = $violation->getMessage();
+            }
+
+            return new JsonResponse(['errors' => $errors], Response::HTTP_BAD_REQUEST);
+        }
+
         $user->setPassword($this->passwordHasher->hashPassword($user, $password));
         $this->em->persist($user);
         $this->em->flush();
